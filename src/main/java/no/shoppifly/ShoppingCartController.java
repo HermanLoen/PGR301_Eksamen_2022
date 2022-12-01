f8582cd39e0e2f20228afe46cee8b10e20a602c5
Herman Loen <h3rmanlo3n@gmail.com>
2022-12-01 14:42:47 +0100

    Good start on implementing metrics in ShoppingCartController

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -1,15 +1,30 @@
 package no.shoppifly;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController()
-public class ShoppingCartController {
+public class ShoppingCartController implements ApplicationListener<ApplicationReadyEvent> {
+
+    private Map<String, NaiveCartImpl> cart = new HashMap();
+    private final CartService cartService;
+    private MeterRegistry meterRegistry;
 
     @Autowired
-    private final CartService cartService;
+    public ShoppingCartController(MeterRegistry meterRegistry, CartService cartService) {
+        this.meterRegistry = meterRegistry;
+        this.cartService = cartService;
+    }
 
     public ShoppingCartController(CartService cartService) {
         this.cartService = cartService;
@@ -48,8 +63,27 @@ public class ShoppingCartController {
      */
     @GetMapping(path = "/carts")
     public List<String> getAllCarts() {
-        return cartService.getAllsCarts();
+        meterRegistry.counter("carts").increment();
+        return cartService.getAllCarts();
     }
 
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
 
+        // Verdi av total
+        Gauge.builder("carts", cart,
+                b -> b.values().size()).register(meterRegistry);
+
+        // Denne meter-typen "Gauge" rapporterer hvor mye penger som totalt finnes i banken
+        Gauge.builder("cartsvalue", cart,
+                        b -> b.values()
+                                .stream()
+                                .map(NaiveCartImpl::total)
+                                .mapToDouble(Float::doubleValue)
+                                .sum())
+                .register(meterRegistry);
+    }
+    @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "account not found")
+    public static class AccountNotFoundException extends RuntimeException {
+    }
 }
