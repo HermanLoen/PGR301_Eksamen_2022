1d3b0c1f8230532da50c3e4044053695891d6e23
Herman Loen <h3rmanlo3n@gmail.com>
2022-12-06 15:43:21 +0100

    added metrics to dashboards.tf

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
   ## Jim; seriously! we can use any word here.. How cool is that?
-  dashboard_body = <<CARTDASHBOARD
+  dashboard_body = <<CART_DASHBOARD
 {
   "widgets": [
     {
@@ -41,8 +41,46 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "Total sum in carts"
       }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkouts.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total number of checkouts"
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "checkout_latency.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Checkout latency"
+      }
     }
   ]
 }
-CARTDASHBOARD
+CART_DASHBOARD
 }
