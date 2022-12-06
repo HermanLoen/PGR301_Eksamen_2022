35db948aad36fc8fd6c33d9a2ec71c114172ea2e
Herman Loen <h3rmanlo3n@gmail.com>
2022-12-06 14:50:41 +0100

    changed bucket name

--- infra/provider.tf
+++ infra/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "pgr301-exam-terraform-state"
+    bucket = "analytics-1051"
     key    = "1051/app-runner-exam.tfstate"
     region = "eu-west-1"
   }
