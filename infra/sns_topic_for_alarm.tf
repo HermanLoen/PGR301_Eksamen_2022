507c269e4c66b7172e0e40fbdcf137dbe32c3faf
Herman Loen <h3rmanlo3n@gmail.com>
2022-12-06 16:19:02 +0100

    added alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "cartsCountNotOverThreshold" {
+  alarm_name                = "carts-sum-not-over-threshold-in-3-repeating-periods"
+  namespace                 = "1051"
+  metric_name               = "carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of carts exceeds 5 in 3 repeating periods of 5 minutes"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
